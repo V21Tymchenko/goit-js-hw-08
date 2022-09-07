1c951b7d7b5cfa7aa2a8a4e413a277ef8e834a21
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-09-07 19:03:42 +0300

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log(SimpleLightbox);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const divRef = document.querySelector('.gallery');
+
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
+divRef.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+divRef.addEventListener('click', onItemClick);
+
+function createGalleryMarkUp(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+let gallery = new SimpleLightbox('.gallery a', {
+  nav: true,
+  close: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onItemClick(evt) {
+  // заборона відкривання у новому вікні картинки по посиланню
+  evt.preventDefault();
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import * as Player from '@vimeo/player/dist/player.js';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
